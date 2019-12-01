27e23599fb5af4a6ad1b34f75d30893ab436713b
MuhammadIsmailShahzad <57398621+MuhammadIsmailShahzad@users.noreply.github.com>
2019-12-02 02:26:37 +0500

    Update process.py

--- process.py
+++ process.py
@@ -1,28 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-import re
 
-website_url = requests.get('https://en.wikipedia.org/wiki/List_of_mobile_network_operators').text
-#from bs4 import BeautifulSoup
-soup = BeautifulSoup(website_url,'lxml')
-#print(soup.prettify())
-ti = 0
+#defining constants
+website_url = requests.get('https://en.wikipedia.org/wiki/List_of_mobile_netwo\
+rk_operators').text
 
-My_table = soup.find('table',{'class':'wikitable sortable'})
-headers = ['Rank','Company','Main Markets','Technology','Total Subscription','Ownership']
-with open('telecom-operators.csv', 'w', newline='', encoding="utf-8") as f:
-    writer = csv.writer(f, delimiter=',', lineterminator='\n')
-    writer.writerow(headers)
-    for tr in My_table('tr'):
-    	row = [t.get_text(strip=True).strip('[List]') for t in tr(['td'])]
-    	if row :
-            cell1 = row[0]
-            cell2 = row[1].replace('[', '').replace(']', '')
-            cell3 = row[2].replace('[', '').replace(']', '')
-            cell4 = row[3].replace('[', '').replace(']', '')
-            cell5 = row[4].replace('[','').replace(']','')
-            cell6 = row[4].replace('[','').replace(']','')
-            row = [cell1,cell2,cell3,cell4,cell5,cell6]
-            writer.writerow(row)
+
+def retrieve():
+    soup = BeautifulSoup(website_url,'lxml')
+    soup_table = soup.find('table',{'class':'wikitable sortable'})
+    return soup_table
+    
+def transform_load(table):
+
+    # Setting the headers for the csv
+    headers = ['Rank','Company','Main Markets','Technology','Total Subscription','Ownership']
+    #Cleaning and writing telecom-operators.csv
+    with open('telecom-operators.csv', 'w', newline='', encoding="utf-8") as f:
+        writer = csv.writer(f, delimiter=',', lineterminator='\n')
+        writer.writerow(headers)
+        for tr in table('tr'):
+            row = [t.get_text(strip=True).strip('[List]') for t in tr(['td'])]
+            if row :
+                cell1 = row[0]
+                cell2 = row[1].replace('[','').replace(']','')
+                cell3 = row[2].replace('[','').replace(']','')
+                cell4 = row[3].replace('[','').replace(']','')
+                cell5 = row[4].replace('[','').replace(']','')
+                cell6 = row[5].replace('[','').replace(']','')
+                row = [cell1,cell2,cell3,cell4,cell5,cell6]
+                writer.writerow(row)
         
+def run():
+    table = retrieve()
+    transform_load(table)
+
+if __name__ == '__main__':
+    run()
